68556117e7b5ab3dae2ebe0cbe3fde92ab5059fb
Mia <113086524+MiaYun98@users.noreply.github.com>
2022-10-11 13:06:54 -0700

    html basic and CSS

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,45 @@
 console.log("hello")
+
+// quote
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '8ffca2429dmsh13d60be90c10c46p18d30bjsn600828036fb6',
+// 		'X-RapidAPI-Host': 'love-quote.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://love-quote.p.rapidapi.com/lovequote', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+// watchmode 
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'X-RapidAPI-Key': '8ffca2429dmsh13d60be90c10c46p18d30bjsn600828036fb6',
+//         'X-RapidAPI-Host': 'watchmode.p.rapidapi.com'
+//     }
+// };
+
+// fetch('https://watchmode.p.rapidapi.com/releases?start_date=20220301&end_date=20220312&limit=250', options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
+
+// Love calculator
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '8ffca2429dmsh13d60be90c10c46p18d30bjsn600828036fb6',
+// 		'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+var submitBtn = $()
