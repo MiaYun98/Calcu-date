0e2184496633fe9ee4cf90de3cc972d689e2794d
Mia <113086524+MiaYun98@users.noreply.github.com>
2022-10-12 22:07:42 -0700

    some touch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // variable setting 
-var submitBtn = $("#search-form"); 
+var submitBtn = $("#search-form");
 var reloadBtn = $("#btn-front");
 var tabBtn = $('.tabBtn');
 var yourNameEl = document.querySelector(".form-input-your");
@@ -17,7 +17,6 @@ function gettingInput(event) {
     var yourName = yourNameEl.value.trim();
     var crushName = crushNameEl.value.trim();
     if (!yourName || !crushName) {
-        alert("you should write something!")
         return;
     }
 
@@ -36,25 +35,25 @@ function getCalculate(yourName, crushName) {
         }
     };
 
-    fetch('https://love-calculator.p.rapidapi.com/getPercentage?sname=' + yourName + "&fname=" + crushName , options)
+    fetch('https://love-calculator.p.rapidapi.com/getPercentage?sname=' + yourName + "&fname=" + crushName, options)
         .then(function (response) {
             if (response.ok) {
                 response.json()
-                .then(function (data) {
-                    printingresult(data);
-                });
-            }   
+                    .then(function (data) {
+                        printingresult(data);
+                    });
+            }
         })
         .catch(function (error) {
             alert('unable to connect to the data')
         })
 }
 
+// result of the love - calculator
 function printingresult(data) {
-    console.log(data);
     resultPage.classList.remove("hidden")
-    var headerEl = document.querySelector(".header"); 
-    headerEl.textContent = (data.sname + " and " + data.fname);
+    var headerEl = document.querySelector(".header");
+    headerEl.textContent = (data.sname.charAt(0).toUpperCase() + data.sname.slice(1) + "    &    " + data.fname.charAt(0).toUpperCase() + data.fname.slice(1));
     var percentageEl = document.querySelector(".percentage");
     percentageEl.textContent = (data.percentage + "%");
     resultnum = data.percentage;
@@ -62,6 +61,7 @@ function printingresult(data) {
     resultSenEl.textContent = ("Result: " + data.result);
 }
 
+// clicks for the tab button on the second page
 function gettingRecommendation(event) {
     var searching = event.target.getAttribute('data-info');
     if (searching === 'movie') {
@@ -74,6 +74,7 @@ function gettingRecommendation(event) {
     }
 }
 
+// having a recommendation according to the percentage of the match
 function gettingMovie() {
     if (resultNum === 0) {
         recommendGenre = 27;
@@ -83,7 +84,7 @@ function gettingMovie() {
         recommendGenre = 10751;
     } else if (resultNum <= 75 && resultNum > 50) {
         recommendGenre = 35;
-    } else if (resultNum < 100 && resultNum >75) {
+    } else if (resultNum < 100 && resultNum > 75) {
         recommendGenre = 10749;
     } else if (resultNum === 100) {
         recommendGenre = 28;
@@ -96,28 +97,31 @@ function gettingMovie() {
             'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
         }
     };
-    
+
     fetch('https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=' + recommendGenre + '&page=1&output_language=en&language=en', options)
-    .then(function (response) {
-        if (response.ok) {
-            response.json()
-            .then(function (data) {
-                contentMovie(data);
-            });
-        }   
-    })
-    .catch(function (error) {
-        alert('unable to connect to the data')
-    })
+        .then(function (response) {
+            if (response.ok) {
+                response.json()
+                    .then(function (data) {
+                        contentMovie(data);
+                    });
+            }
+        })
+        .catch(function (error) {
+            alert('unable to connect to the data')
+        })
 }
 
+// pringint the movie list into the box
 function contentMovie(data) {
     contentListEl.textContent = "";
-    for (var i = 0; i < data.results.length; i++ ) {
+    for (var i = 0; i < data.results.length; i++) {
         var li = document.createElement('li');
         var a = document.createElement("a");
         var img = document.createElement("img");
+        img.setAttribute("class", "movie-img")
         a.setAttribute("href", data.results[i].streamingInfo.netflix.us.link);
+        a.setAttribute("target", "_blank");
         img.setAttribute("src", data.results[i].posterURLs.original);
         a.appendChild(img)
         li.appendChild(a);
@@ -125,29 +129,31 @@ function contentMovie(data) {
     }
 }
 
+// rendom quote generating API
 function gettingQuote() {
     const options = {
-    	method: 'GET',
-    	headers: {
-    		'X-RapidAPI-Key': '8ffca2429dmsh13d60be90c10c46p18d30bjsn600828036fb6',
-    		'X-RapidAPI-Host': 'love-quote.p.rapidapi.com'
-    	}
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '8ffca2429dmsh13d60be90c10c46p18d30bjsn600828036fb6',
+            'X-RapidAPI-Host': 'love-quote.p.rapidapi.com'
+        }
     };
 
     fetch('https://love-quote.p.rapidapi.com/lovequote', options)
         .then(function (response) {
             if (response.ok) {
                 response.json()
-                .then(function (data) {
-                    printingquote(data);
-                });
-            }   
+                    .then(function (data) {
+                        printingquote(data);
+                    });
+            }
         })
         .catch(function (error) {
             alert('unable to connect to the data')
         })
 }
 
+// printing quotes to the HTML so they show up to the content box
 function printingquote(data) {
     contentListEl.textContent = "";
     var li = document.createElement('li');
@@ -158,7 +164,7 @@ function printingquote(data) {
     } else {
         h4.textContent = (data.quote);
     }
-    
+
     var p = document.createElement('p');
     p.textContent = (data.author);
     li.appendChild(h4);
@@ -166,12 +172,11 @@ function printingquote(data) {
     contentListEl.appendChild(li);
 }
 
-
-
 // frontPage.classList.add("hidden");
 resultPage.classList.add("hidden");
 submitBtn.on('submit', gettingInput);
 reloadBtn.on('click', function () {
     location.reload();
 });
+// for the tab
 tabBtn.on('click', gettingRecommendation)
